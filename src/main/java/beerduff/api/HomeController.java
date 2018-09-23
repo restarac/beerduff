e0d492dc9bb69d6c1be42fc73c7565e547d0fe97
Frederico Pereira <frederico.silva.pereira@gmail.com>
2018-09-23 17:31:15 +0000

    initializing the project

---
+++ src/main/java/beerduff/api/HomeController.java
@@ -0,0 +1,13 @@
+package beerduff.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@RequestMapping("/")
+	public String home() {
+		return "Hello Docker World";
+	}
+}
