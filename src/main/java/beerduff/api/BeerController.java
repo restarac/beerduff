2299b736990ace137d73fca7c2481e17badec198
Frederico Pereira <frederico.silva.pereira@gmail.com>
2018-09-28 01:07:27 -0300

    Change controller to respond 400 and 404 in case of no beer are found

--- src/main/java/beerduff/api/BeerController.java
+++ src/main/java/beerduff/api/BeerController.java
@@ -7,27 +7,37 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.hateoas.Resource;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @RepositoryRestController
 public class BeerController {
 
-	private final BeerRepository repository;
+    private final BeerRepository repository;
 
-	@Autowired
-	public BeerController(BeerRepository repo) {
-		repository = repo;
+    @Autowired
+    public BeerController(BeerRepository repo) {
+	repository = repo;
+    }
+
+    @RequestMapping(method = GET, value = "/beers/search/findBestPlaylistBeer")
+    public @ResponseBody ResponseEntity<?> findByAverageTemperature(String temperature) {
+	if (StringUtils.isEmpty(temperature)) {
+	    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("missing param 'temperature'");
 	}
 
-	@RequestMapping(method = GET, value = "/beers/search/findBestPlaylistBeer")
-	public @ResponseBody ResponseEntity<?> findByAverageTemperature(String temperature) {
-		List<Beer> beers = repository.findByAverageTemperature(-2);
+	List<Beer> beers = repository.findByAverageTemperature(Integer.parseInt(temperature));
 
-		Resource<Beer> resources = new Resource<Beer>(beers.get(0));
-
-		return ResponseEntity.ok(resources);
+	if (beers.isEmpty()) {
+	    return ResponseEntity.status(HttpStatus.NOT_FOUND).body("no beers found!");
 	}
+
+	Resource<Beer> resources = new Resource<Beer>(beers.get(0));
+
+	return ResponseEntity.ok(resources);
+    }
 
 }
