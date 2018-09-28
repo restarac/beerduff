ad270d3c12e7ed257bdd37aeaf89949077a61581
Frederico Pereira <frederico.silva.pereira@gmail.com>
2018-09-28 01:12:31 -0300

    Trying new query on repository

--- src/main/java/beerduff/api/BeerRepository.java
+++ src/main/java/beerduff/api/BeerRepository.java
@@ -10,7 +10,7 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 @RepositoryRestResource
 public interface BeerRepository extends PagingAndSortingRepository<Beer, Long> {
 
-	@Query(value = "SELECT b.* FROM Beer b WHERE (:temp BETWEEN b.min_temp AND b.max_temp) ORDER BY b.average_temp, b.name", nativeQuery = true)
-	List<Beer> findByAverageTemperature(@Param("temp") int temperature);
+    @Query(value = "SELECT b.* FROM Beer b WHERE (:temp BETWEEN b.min_temp AND b.max_temp) ORDER BY ABS(b.average_temp - :temp), b.name", nativeQuery = true)
+    List<Beer> findByAverageTemperature(@Param("temp") int temperature);
 
 }
