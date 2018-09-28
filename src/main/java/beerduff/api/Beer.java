b64a22f5fbd03dee62faf60135a88128aecc75a7
Frederico Pereira <frederico.silva.pereira@gmail.com>
2018-09-28 01:06:41 -0300

    fix param order on constructor

--- src/main/java/beerduff/api/Beer.java
+++ src/main/java/beerduff/api/Beer.java
@@ -10,69 +10,70 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Beer {
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
 
-	@Column(name = "name", nullable = false)
-	private String name;
-	@Column(name = "max_temp", nullable = false)
-	private int maxTempInCelsius;
-	@Column(name = "min_temp", nullable = false)
-	private int minTempInCelsius;
+    @Column(name = "name", nullable = false)
+    private String name;
+    @Column(name = "max_temp", nullable = false)
+    private int maxTempInCelsius;
+    @Column(name = "min_temp", nullable = false)
+    private int minTempInCelsius;
 //	@JsonIgnore
-	@Column(name = "average_temp", nullable = false)
-	private int averageTempInCelsius;
+    @Column(name = "average_temp", nullable = false)
+    private int averageTempInCelsius;
 
-	public Beer() {
-		super();
-	}
+    public Beer() {
+	super();
+    }
 
-	public Beer(String name, int maxTemp, int minTemp) {
-		super();
-		this.name = name;
-		this.maxTempInCelsius = maxTemp;
-		this.minTempInCelsius = minTemp;
-		calcAverageTempInCelsius();
-	}
+    public Beer(String name, int minTemp, int maxTemp) {
+	super();
+	this.name = name;
+	this.maxTempInCelsius = maxTemp;
+	this.minTempInCelsius = minTemp;
+	calcAverageTempInCelsius();
+    }
 
-	public int getMaxTemp() {
-		return maxTempInCelsius;
-	}
+    public int getMaxTemp() {
+	return maxTempInCelsius;
+    }
 
-	public void setMaxTemp(int maxTemp) {
-		this.maxTempInCelsius = maxTemp;
-		calcAverageTempInCelsius();
-	}
+    public void setMaxTemp(int maxTemp) {
+	this.maxTempInCelsius = maxTemp;
+	calcAverageTempInCelsius();
+    }
 
-	public int getMinTemp() {
-		return minTempInCelsius;
-	}
+    public int getMinTemp() {
+	return minTempInCelsius;
+    }
 
-	public void setMinTemp(int minTemp) {
-		this.minTempInCelsius = minTemp;
-		calcAverageTempInCelsius();
-	}
+    public void setMinTemp(int minTemp) {
+	this.minTempInCelsius = minTemp;
+	calcAverageTempInCelsius();
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+	return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+	this.name = name;
+    }
 
-	public int getAverageTempInCelsius() {
-		return averageTempInCelsius;
-	}
+    public int getAverageTempInCelsius() {
+	return averageTempInCelsius;
+    }
 
-	private void calcAverageTempInCelsius() {
-		this.averageTempInCelsius = (this.minTempInCelsius + this.maxTempInCelsius) / 2;
-	}
+    private void calcAverageTempInCelsius() {
+	int sumTemp = (this.minTempInCelsius + this.maxTempInCelsius);
+	this.averageTempInCelsius = Math.round(sumTemp / 2);
+    }
 
-	@Override
-	public String toString() {
-		return "Beer [id=" + id + ", name=" + name + ", maxTempInCelsius=" + maxTempInCelsius + ", minTempInCelsius="
-				+ minTempInCelsius + "]";
-	}
+    @Override
+    public String toString() {
+	return "Beer [id=" + id + ", name=" + name + ", maxTempInCelsius=" + maxTempInCelsius + ", minTempInCelsius="
+		+ minTempInCelsius + "]";
+    }
 }
